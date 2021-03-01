658ac1c2bf2fa4f541d7f70ff19030acd1a0de27
--add <andrewksmoker@gmail.com>
2021-03-01 10:52:04 -0500

    commit tree 5f467b84b3c60c72ae2bb654071ac1361a0743f9

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={"I am sad"} />
   </div>,
   document.getElementById('root')
 );
